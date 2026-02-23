cb60d16151c0d311fbdc371e77590ae6a4737b54
Shaurya Yadav <shaurya.y321@gmail.com>
2026-02-23 23:37:03 +0530

    dsa: solved 2 problems with array that too by using LLMs, dev: revised callback, hoping to get things done today but messed up

--- DSA/week-4_arrays/contest/template.cpp
+++ DSA/week-4_arrays/contest/template.cpp
@@ -1,36 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void inputArray(int arr[], int n) {
+void inputArray(vector<int>& arr, int n) {
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
 }
 
-void outputArray(int arr[], int n) {
+void outputArray(vector<int>& arr, int n) {
   for (int i = 0; i < n; i++) {
     cout << arr[i] << " ";
   }
 }
 
-void ArrangeNum() {
-  int n;
-  cin >> n;
-  int arr[n];
+void swipe(int& a, int& b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
 
-  for (int i = 0; i < n / 2; i++) {
-    arr[i] = 2 * i + 1;
+void swipeAlternate(int n) {
+  vector<int> arr(n);
+  inputArray(arr, n);
+  for (int i = 0; i < n-1; i += 2) {
+    swipe(arr[i], arr[i + 1]);
   }
-  int even = 2;
-  for (int j = n - 1; j >= n / 2; j--) {
-    arr[j] = even;
-    even += 2;
-  }
-
   outputArray(arr, n);
 }
 
 int main() {
-  ArrangeNum();
+  int n;
+  cin >> n;
+
+  vector<int> input(n);
+  inputArray(input, n);
+
+  for (int i = 0; i < n; i++) {
+    swipeAlternate(input[i]);
+    cout<<"\n";
+  }
   return 0;
 }
