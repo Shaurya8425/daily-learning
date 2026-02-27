6211cb3fe0d525dcc092584673e537a1af9371e2
Shaurya Yadav <shaurya.y321@gmail.com>
2026-02-27 23:53:25 +0530

    dsa: completed 2d arrays slide, dev: improved github contribution graph & added some stat tiles

--- DSA/week-5_strings-2d-arrays/2d-arrays.cpp
+++ DSA/week-5_strings-2d-arrays/2d-arrays.cpp
@@ -27,7 +27,7 @@ int main() {
   } */
 
   /* if u want to print by column just interchange n,m & i,j */
-  for (int j = 0; j < m; j++) {
+  /* for (int j = 0; j < m; j++) {
     if (j % 2 == 0) {
       for (int i = 0; i < n; i++) {
         cout << arr[i][j] << " ";
@@ -37,6 +37,41 @@ int main() {
         cout << arr[i][j] << " ";
       }
     }
+  } */
+
+  /* for (int j = 0; j < m; j++) {
+    cout << arr[0][j] << " ";
+  }
+  for (int i = 1; i < n; i++) {
+    cout << arr[i][m - 1] << " ";
+  }
+  for (int j = m - 2; j >= 0; j--) {
+    cout << arr[n - 1][j] << " ";
+  }
+  for (int i = n - 2; i >= 1; i--) {
+    cout << arr[i][0] << " ";
+  } */
+
+  int index = INT_MIN;
+  int maxCount = 0;
+  vector<int> indexArr;
+  for (int i = 0; i < n; i++) {
+    int count = 0;
+    for (int j = 0; j < m; j++) {
+      if (arr[i][j] == 1) count++;
+    }
+    if (count > maxCount) {
+      maxCount = count;
+      indexArr.clear();
+      indexArr.push_back(i);
+      index = i;
+    } else if (count == maxCount) {
+      indexArr.push_back(i);
+    }
+  }
+  cout << index << endl;
+  for (size_t i = 0; i < indexArr.size(); i++) {
+    cout << indexArr[i]<<" ";
   }
   return 0;
 }
