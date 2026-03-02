7fea7ccf516c5d7f8cff81b190e0a0e5a552ae6b
Shaurya Yadav <shaurya.y321@gmail.com>
2026-03-02 23:37:22 +0530

    dsa: solved majority of strings Qs on codeForces, dev: didn't do anything | hoping to dive into preCICE org

---
+++ DSA/week-5_strings-2d-arrays/contest/CF-strings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string password(string s) {
+  for (int i = 0; i < (int)s.length(); i++) {
+     
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  return 0;
+}
